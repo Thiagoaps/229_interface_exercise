2ea81548b1b26811b2c4ec845b8f34a82ccf36af
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-05 23:26:08 -0300

    interface exercise, before correction

---
+++ src/model/service/ContractService.java
@@ -0,0 +1,36 @@
+package model.service;
+
+import java.time.LocalDate;
+
+import model.entities.Contract;
+import model.entities.Installment;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	public OnlinePaymentService getOnlinePaymentService() {
+		return onlinePaymentService;
+	}
+
+	public void setOnlinePaymentService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int months) {
+		double basicQuota = contract.getTotalValue() / months;
+		
+		for (int i=1; i<=months; i++) {			
+			LocalDate dueDate = contract.getDate().plusMonths(i);
+			double interest = onlinePaymentService.interest(basicQuota, i);
+			double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+			double quota = basicQuota + interest + fee;
+			
+			contract.getInstallment().add(new Installment(dueDate, quota));
+		}		
+	}
+}
